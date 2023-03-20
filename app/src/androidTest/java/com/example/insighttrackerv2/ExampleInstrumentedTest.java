e9dbf1d3f8039551035fadb36bfc6e8911ff7f8a
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-20 22:11:49 +0530

    Add UI Automator & Test Successfully!

--- app/src/androidTest/java/com/example/insighttrackerv2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/insighttrackerv2/ExampleInstrumentedTest.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.core.app.ActivityScenario;
+
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,5 +25,27 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.insighttrackerv2", appContext.getPackageName());
+    }
+
+    @Test
+    public void minimizeAndRelaunchApp() {
+        // Launch the activity to be tested
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+
+        // Press the home button to minimize the app
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        device.pressHome();
+
+        // Wait for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Relaunch the activity
+        scenario = ActivityScenario.launch(MainActivity.class);
+
+        // Add any assertions or checks here
     }
 }
